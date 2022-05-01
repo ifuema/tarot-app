fd3fe86c87451f6ed4dbb76454d57f187e04932f
ifuema <y3bpcy3bpc@gmail.com>
2022-05-01 16:19:40 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+import router from './router'
+import axios from 'axios'
+Vue.prototype.$axios = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
